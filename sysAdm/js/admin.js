55874564fdafd7190cbe0b10c8e8091b92cb1ad3
lanker <lanker@test0023.test0012.dlinkddns.com>
2015-10-22 08:24:02 +0800

    admin load left.html and header.html, implement template

--- sysAdm/js/admin.js
+++ sysAdm/js/admin.js
@@ -1,11 +1,18 @@
 
 AdminRoutes = Backbone.Router.extend({
     initialize : function() {
+	$("#left").load("left.html");
+	$("#header").load("header.html");
     },
     routes : {
-	'newsAdmLeft' : 'allBlogShow'
+	'productList' : 'productList'
     },
-    allBlogShow : function() {
-	console.log('test');
+    productList : function() {
+	$("#right").load("prodAdm/prodList.html", function() {
+	    var tem = _.template($("#right > script").html());
+	    $("#right tbody").html(tem());
+	    $("#left a").removeClass('clicked');
+	    $("#left a[href=#productList]").addClass('clicked');
+	});
     }
 });
