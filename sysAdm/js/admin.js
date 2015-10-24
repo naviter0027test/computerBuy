b3425a148e53e0905d2b200c0c256ef2567c482b
lanker <lanker@test0023.test0012.dlinkddns.com>
2015-10-24 11:15:26 +0800

    login and logout js connect php and success to jump page

--- sysAdm/js/admin.js
+++ sysAdm/js/admin.js
@@ -5,7 +5,8 @@ AdminRoutes = Backbone.Router.extend({
 	$("#header").load("header.html");
     },
     routes : {
-	'productList' : 'productList'
+	'productList' : 'productList',
+	'logout' : 'logout'
     },
     productList : function() {
 	$("#right").load("prodAdm/prodList.html", function() {
@@ -13,6 +14,19 @@ AdminRoutes = Backbone.Router.extend({
 	    $("#right tbody").html(tem());
 	    $("#left a").removeClass('clicked');
 	    $("#left a[href=#productList]").addClass('clicked');
+	});
+    },
+    logout : function() {
+	var postData = {};
+	postData['instr'] = "logout";
+	$.post("instr.php", postData, function(data, status) {
+	    if(status == "success") {
+		data = JSON.parse(data);
+		if(data['status'] == 200) {
+		    alert("成功登出");
+		    location.href = "login.html";
+		}
+	    }
 	});
     }
 });
---
+++ sysAdm/login/Login.js
@@ -0,0 +1,43 @@
+View = Backbone.View.extend({
+    initialize : function() {
+    },
+    el : "#login",
+    
+    events : {
+	"click button" : "login"
+    },
+
+    login : function(evt) {
+	this.model.set("el", this.$el);
+	this.model.login();
+	return false;
+    }
+});
+
+Model = Backbone.Model.extend({
+    initialize : function() {
+    },
+    defaults : {
+	el : null,
+	data : null
+    },
+    login : function() {
+	var el = this.get("el");
+	el.ajaxSubmit(function(data, status) {
+	    if(status == "success") {
+		data = JSON.parse(data);
+		if(data['status'] == 500) 
+		    if(data['msg'] == "已經登入了") {
+			alert(data['msg']);
+			location.href = "admin.html";
+		    }
+		    else
+			;
+		else if(data['status'] == 200) {
+		    alert("成功登入");
+		    location.href = "admin.html";
+		}
+	    }
+	});
+    }
+});
