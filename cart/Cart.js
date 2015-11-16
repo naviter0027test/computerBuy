bf50d9c2562ce0ab8318a25a815c1ac9a2b6e254
lanker <lanker@test0023.test0012.dlinkddns.com>
2015-11-16 22:49:33 +0800

    cart function write into Cart.js

---
+++ cart/Cart.js
@@ -0,0 +1,30 @@
+Cart = Backbone.View.extend({
+    initialize : function() {
+	console.log('view test');
+	console.log(this.$el);
+	this.template = _.template($("#prodCart").html());
+	this.render();
+    },
+    el : '',
+    template : null,
+    render : function() {
+	var temData = this.model.get('data');
+	this.$el.html(this.template(temData));
+    }
+});
+
+CartStore = Backbone.Model.extend({
+    initialize : function() {
+	var cart = [];
+	if(localStorage.getItem('cart') != null)
+	    cart = JSON.parse(localStorage.getItem('cart'));
+	console.log(cart);
+	var temData = {};
+	temData['cart'] = cart;
+	this.set('data', temData);
+	//template = _.template($("#prodCart").html())
+    },
+    defaults : {
+	data : null
+    }
+});
