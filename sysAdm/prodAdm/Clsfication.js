99b47a6626b423a666982a6979d0b4543965ad88
lanker <lanker@test0023.test0012.dlinkddns.com>
2015-11-17 08:10:24 +0800

    class del finish

--- sysAdm/prodAdm/Clsfication.js
+++ sysAdm/prodAdm/Clsfication.js
@@ -7,11 +7,25 @@ ClsList = Backbone.View.extend({
 	    self.render();
 	});
     },
+    events : {
+	'click a' : 'operating'
+    },
     el : '',
     template : null,
     render : function() {
 	var data = this.model.get("data");
 	this.$el.html(this.template(data));
+    },
+
+    operating : function(evt) {
+	var btn = evt.target;
+	var data = this.model.get("data");
+	var pos = $(btn).attr("pos");
+	var oper = $(btn).attr("oper");
+	if(oper == "del") {
+	    this.model.clsDel(data['data'][pos]['c_id'])
+	}
+	return false;
     }
 });
 
@@ -31,8 +45,26 @@ ClsModel = Backbone.Model.extend({
 	});
     },
     clsAdd : function(form) {
-	$(form).ajaxSubmit(function(data) {
+	$("#clsAdd").ajaxSubmit(function(data) {
+	    data = JSON.parse(data);
 	    console.log(data);
+	    if(data['status'] == 200)
+		alert("新增成功");
+	    else 
+		alert("新增失敗");
+	});
+    },
+    clsDel : function(c_id) {
+	var self = this;
+	var postData = {};
+	postData['instr'] = "classDel";
+	postData['c_id'] = c_id;
+	$.post("instr.php", postData, function(data) {
+	    data = JSON.parse(data);
+	    if(data['status'] == 200) {
+		alert("刪除成功");
+		self.clsAll();
+	    }
 	});
     }
 });
