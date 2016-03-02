b880c547ac545fe3d9640e968112cf38d6ca91ed
lanker <lanker@test0023.test0012.dlinkddns.com>
2016-03-02 10:31:10 +0800

    order result css redesign

--- js/orderResult.js
+++ js/orderResult.js
@@ -2,4 +2,6 @@ $(document).ready(function() {
     $("#template").load("template/orderResult.html", function() {
         var order = new OrderShow({'model' : new OrderModel(), 'el' : "#orderShow"});
     });
+    $("#header").load("template/header.html");
+    $("#footer").load("template/footer.html");
 });
