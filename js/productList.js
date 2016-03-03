d40961fd9cacf0173675b6d087c87599a0b53ea5
lanker <lanker@test0023.test0012.dlinkddns.com>
2016-03-03 09:52:17 +0800

    product infomation page add to cart finish, go to cart page button css redesign

--- js/productList.js
+++ js/productList.js
@@ -1,13 +1,15 @@
 $(document).ready(function() {
-    var prodList = new ProductList({'model' : new ProdModel(), 'el' : "#content"});
-    var prodcls = new ClsShow({'model' : new ProdCls(), 'el' : "#leftMenu" });
-    //var topMenu = new ClsTop({'model' : new ProdCls(), 'el' : "#topMenu" });
-    prodcls.model.set('prodList', prodList);
-    //topMenu.model.set('prodList', prodList);
+    $("#scriptLoad").load("template/productList.html", function() {
+        var prodList = new ProductList({'model' : new ProdModel(), 'el' : "#content"});
+        var prodcls = new ClsShow({'model' : new ProdCls(), 'el' : "#leftMenu" });
+        //var topMenu = new ClsTop({'model' : new ProdCls(), 'el' : "#topMenu" });
+        prodcls.model.set('prodList', prodList);
+        //topMenu.model.set('prodList', prodList);
 
-    $("#header").load("template/header.html",function() {
-        $("#nav a[href='productList.html']").addClass("choosed");
+        $("#header").load("template/header.html",function() {
+            $("#nav a[href='productList.html']").addClass("choosed");
+        });
+        $("#footer").load("template/footer.html");
     });
-    $("#footer").load("template/footer.html");
 });
 
